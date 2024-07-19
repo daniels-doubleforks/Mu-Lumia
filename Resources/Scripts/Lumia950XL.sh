c276445123fdf2a3ad08ef6826a7c20f022bceca
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-07-20 01:36:25 +0200

    add LdScript

--- Resources/Scripts/Lumia950XL.sh
+++ Resources/Scripts/Lumia950XL.sh
@@ -4,5 +4,5 @@
 cat "./Build/Lumia950XLPkg/DEBUG_CLANGPDB/FV/LUMIA950XL_UEFI.fd" > ./UEFI.fd||exit 1
 # Copy UEFI FD File to a ELF Payload File
 aarch64-linux-gnu-objcopy -I binary -O elf64-littleaarch64 --binary-architecture aarch64 "./Build/${TARGET_DEVICE}Pkg/DEBUG_CLANGPDB/FV/${TARGET_DEVICE^^}_UEFI.fd" "./Build/${TARGET_DEVICE}Pkg/DEBUG_CLANGPDB/FV/${TARGET_DEVICE^^}_UEFI.o"||_error "\nFailed to Build ELF Payload!\n"
-aarch64-linux-gnu-ld -m aarch64elf "./Build/${TARGET_DEVICE}Pkg/DEBUG_CLANGPDB/FV/${TARGET_DEVICE^^}_UEFI.o" -o "./Mu-${TARGET_DEVICE}.elf"||_error "\nFailed to Build ELF Payload!\n"
+aarch64-linux-gnu-ld -m aarch64elf "./Build/${TARGET_DEVICE}Pkg/DEBUG_CLANGPDB/FV/${TARGET_DEVICE^^}_UEFI.o" -T "./FvWrapper.ld" -o "./Mu-${TARGET_DEVICE}.elf"||_error "\nFailed to Build ELF Payload!\n"
 

38e25a936a7c7ddbea0011ae89f8656ed3c93b75
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-07-19 21:41:50 +0200

    Create PlatformPrePiLib.c

---
+++ Silicon/Qualcomm/MSM8994Pkg/Library/PlatformPrePiLib/PlatformPrePiLib.c
@@ -0,0 +1,15 @@
+#include <Library/IoLib.h>
+#include <Library/PlatformPrePiLib.h>
+#include <Library/DevicePrePiLib.h>
+
+#include "PlatformRegisters.h"
+
+VOID
+PlatformInitialize ()
+{
+  // Disable WatchDog Timer
+  MmioWrite32 (APSS_WDT_BASE + APSS_WDT_ENABLE_OFFSET, 0x0);
+
+  // Run Device Specific Code
+  DeviceInitialize ();
+}

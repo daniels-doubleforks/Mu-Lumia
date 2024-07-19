9a7c4a82b00e21c25e2644fbf8f55efeaed295e0
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-07-20 00:57:53 +0200

    Update Lumia950XL.sh

--- Resources/Scripts/Lumia950XL.sh
+++ Resources/Scripts/Lumia950XL.sh
@@ -4,5 +4,5 @@
 cat "./Build/Lumia950XLPkg/DEBUG_CLANGPDB/FV/LUMIA950XL_UEFI.fd" > ./UEFI.fd||exit 1
 # Copy UEFI FD File to a ELF Payload File
 arm-linux-gnueabi-objcopy -I binary -B ARM -O elf32-littlearm "./Build/${TARGET_DEVICE}Pkg/DEBUG_CLANGPDB/FV/${TARGET_DEVICE^^}_UEFI.fd" "./Build/${TARGET_DEVICE}Pkg/DEBUG_CLANGPDB/FV/${TARGET_DEVICE^^}_UEFI.o"||_error "\nFailed to Build ELF Payload!\n"
-arm-linux-gnueabi-ld -m armelf_linux_eabi "./Build/${TARGET_DEVICE}Pkg/DEBUG_CLANGPDB/FV/${TARGET_DEVICE^^}_UEFI.o" -T "./configs/${TARGET_DEVICE}.ld" -o "./Mu-${TARGET_DEVICE}.elf"||_error "\nFailed to Build ELF Payload!\n"
+arm-linux-gnueabi-ld -m armelf_linux_eabi "./Build/${TARGET_DEVICE}Pkg/DEBUG_CLANGPDB/FV/${TARGET_DEVICE^^}_UEFI.o" -o "./Mu-${TARGET_DEVICE}.elf"||_error "\nFailed to Build ELF Payload!\n"
 

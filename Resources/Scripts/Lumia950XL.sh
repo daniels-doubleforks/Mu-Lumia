c51fc767fdd90f55aacaa32f548773f2ccd04c38
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-07-20 01:30:20 +0200

    Update Lumia950XL.sh

--- Resources/Scripts/Lumia950XL.sh
+++ Resources/Scripts/Lumia950XL.sh
@@ -3,6 +3,6 @@
 # Copy BootShim + UEFI to a Payload File
 cat "./Build/Lumia950XLPkg/DEBUG_CLANGPDB/FV/LUMIA950XL_UEFI.fd" > ./UEFI.fd||exit 1
 # Copy UEFI FD File to a ELF Payload File
-aarch64-linux-gnu-objcopy -I binary -O elf64-little "./Build/${TARGET_DEVICE}Pkg/DEBUG_CLANGPDB/FV/${TARGET_DEVICE^^}_UEFI.fd" "./Build/${TARGET_DEVICE}Pkg/DEBUG_CLANGPDB/FV/${TARGET_DEVICE^^}_UEFI.o"||_error "\nFailed to Build ELF Payload!\n"
+aarch64-linux-gnu-objcopy -I binary -O elf64-littleaarch64 --binary-architecture aarch64 "./Build/${TARGET_DEVICE}Pkg/DEBUG_CLANGPDB/FV/${TARGET_DEVICE^^}_UEFI.fd" "./Build/${TARGET_DEVICE}Pkg/DEBUG_CLANGPDB/FV/${TARGET_DEVICE^^}_UEFI.o"||_error "\nFailed to Build ELF Payload!\n"
 aarch64-linux-gnu-ld -m aarch64elf "./Build/${TARGET_DEVICE}Pkg/DEBUG_CLANGPDB/FV/${TARGET_DEVICE^^}_UEFI.o" -o "./Mu-${TARGET_DEVICE}.elf"||_error "\nFailed to Build ELF Payload!\n"
 

28c5da7b7c1637ff8b979c8e4dd486ad7e28e747
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-07-20 00:31:19 +0200

    Update Lumia950XL.sh

--- Resources/Scripts/Lumia950XL.sh
+++ Resources/Scripts/Lumia950XL.sh
@@ -2,3 +2,7 @@
 
 # Copy BootShim + UEFI to a Payload File
 cat "./Build/Lumia950XLPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/LUMIA950XL_UEFI.fd" > ./UEFI.fd||exit 1
+# Copy UEFI FD File to a ELF Payload File
+${CLANG38_ARM_PREFIX}objcopy -I binary -O elf32-littlearm --binary-architecture arm "./Build/${TARGET_DEVICE}Pkg/${_TARGET_BUILD_MODE}_CLANG38/FV/${TARGET_DEVICE^^}_UEFI.fd" "./Build/${TARGET_DEVICE}Pkg/${_TARGET_BUILD_MODE}_CLANG38/FV/${TARGET_DEVICE^^}_UEFI.o"||_error "\nFailed to Build ELF Payload!\n"
+${CLANG38_ARM_PREFIX}ld -m armelf_linux_eabi "./Build/${TARGET_DEVICE}Pkg/${_TARGET_BUILD_MODE}_CLANG38/FV/${TARGET_DEVICE^^}_UEFI.o" -T "./configs/${TARGET_DEVICE}.ld" -o "./Mu-${TARGET_DEVICE}.elf"||_error "\nFailed to Build ELF Payload!\n"
+
